a0bb7f269d3c889610875ffa472f400d56506331
Iskander Dosgali <idosgali@gmail.com>
2022-01-17 23:39:01 +0600

    #2 Add modal class context TaskTrackerContext.cs

---
+++ TaskTracker/TaskTracker/Models/TaskTrackerContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskTracker.Models
+{
+    public class TaskTrackerContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TaskTrackerContext(DbContextOptions<TaskTrackerContext> options) : base(options)
+        {
+        }
+    }
+}
