f500f155236f3ebb60f63887c2254cb804da13f0
Iskander Dosgali <idosgali@gmail.com>
2022-01-18 00:08:06 +0600

    #3 Create Controller TasksController.cs

---
+++ TaskTracker/TaskTracker/Controllers/TasksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskTracker.Models;
+
+namespace TaskTracker.Controllers
+{
+    public class TasksController : Controller
+    {
+        private TaskTrackerContext _db;
+
+        public TasksController(TaskTrackerContext db)
+        {
+            _db = db;
+        }
+        
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
