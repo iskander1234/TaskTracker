387cddfaf4132771d9fcdb8c6052046a551c494d
Iskander Dosgali <idosgali@gmail.com>
2022-01-18 00:03:56 +0600

    #3 Added the required fields to the model and enum Status and Priority

--- TaskTracker/TaskTracker/Models/Task.cs
+++ TaskTracker/TaskTracker/Models/Task.cs
@@ -1,7 +1,33 @@
-﻿namespace TaskTracker.Models
+﻿using System;
+
+namespace TaskTracker.Models
 {
+    public enum Status
+    {
+        New,
+        Open,
+        Close
+    }
+    public enum Priority
+    {
+        Tall,
+        Middle,
+        Low
+    }
+
     public class Task
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; } = Status.New;
+        public bool IsDeleted { get; set; } = false;
+        public Priority Priority { get; set; }
+
+        public DateTime СreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime? ClosedDate { get; set; }
+
+        public DateTime? OpenedDate { get; set; }
     }
 }
